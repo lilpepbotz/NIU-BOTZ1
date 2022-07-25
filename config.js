7b4574c28bd70a634875b6dad6876ab2b95b1300
lilpepbotz <109938232+lilpepbotz@users.noreply.github.com>
2022-07-25 19:03:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,35 +1,35 @@
 //=============『 Utama 』================== //
-global.owner = ['6287898307350'] 
+global.owner = ['6282239202895'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Gempy⛧⁩⁩'
-global.numberowner = '6287898307350'
-global.instagram = 'https://www.instagram.com/gmpyyy'
-global.github = 'https://github.com/gmpyyy'
-global.dana = '087898307350'
-global.pulsa = '087898307350'
-global.gopay = '-'
+global.nameowner = 'Alwi⛧⁩⁩'
+global.numberowner = '6282239202895'
+global.instagram = 'https://www.instagram.com/none'
+global.github = 'https://github.com/lilpepbotz'
+global.dana = '6282239202895'
+global.pulsa = '6282239202895'
+global.gopay = '6282239202895'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'PENTABOT'
-global.gc = 'https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2'
-global.web = 'https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'NIU BOTZ'
+global.gc = 'https://chat.whatsapp.com/D8jEgAYOBGFBIAHRx9JAt9'
+global.web = 'https://chat.whatsapp.com/D8jEgAYOBGFBIAHRx9JAt9' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ini juga ganti 
+global.fotonya1 = 'https://i.postimg.cc/DwmXWdhF/IMG-20220718-WA0195.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://i.postimg.cc/DwmXWdhF/IMG-20220718-WA0195.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'PENTABOT'
+global.wm = 'NIU BOTZ'
 global.watermark = wm
-global.wm2 = '                     「PENTABOT⁩ 」'
-global.wm3 = 'Gempy⛧'
+global.wm2 = '                     「NIU BOTZ⁩ 」'
+global.wm3 = 'Alwi⛧'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg'
+global.media = 'https://i.postimg.cc/DwmXWdhF/IMG-20220718-WA0195.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = 'Process...'
 global.eror = 'ERROR'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'Created by'
-global.author = 'Gempy⛧⁩⁩'
+global.author = 'Alwi⛧⁩⁩'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
